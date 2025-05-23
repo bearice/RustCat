df05eac61d36ec1536de6148a1d1ae41e672d6aa
Bearice Ren <bearice@icybear.net>
2025-05-23 16:18:24 +0900

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
-fn main() {
+use std::{io, path::Path};
+use winres::WindowsResource;
+
+fn main() -> io::Result<()> {
     // Get Git commit hash
     let output = std::process::Command::new("git")
         .args(["rev-parse", "--short", "HEAD"])
@@ -18,49 +21,60 @@ fn main() {
         }
     }
 
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("assets/appIcon.ico")
-            // .set_manifest_file("assets/manifest.xml") // This line removed
-            .compile()
-            .unwrap();
+    let profile = std::env::var("PROFILE").unwrap();
+    if profile == "release" {
+        println!("cargo:rustc-cfg=release");
     }
-    // Generate icons.rs
-    let mut file_content = String::new();
-    file_content.push_str("pub const DARK_CAT: &[&[u8]] = &[\n");
-    for i in 0..5 {
-        file_content.push_str(&format!(
-            "\tinclude_bytes!(\"../../assets/cat/dark_cat_{}.ico\"),\n",
-            i
-        ));
-    }
-    file_content.push_str("];\n");
-    file_content.push_str("pub const LIGHT_CAT: &[&[u8]] = &[\n");
-    for i in 0..5 {
-        file_content.push_str(&format!(
-            "\tinclude_bytes!(\"../../assets/cat/light_cat_{}.ico\"),\n",
-            i
-        ));
-    }
-    file_content.push_str("];\n");
-    file_content.push_str("pub const DARK_PARROT: &[&[u8]] = &[\n");
-    for i in 0..10 {
-        file_content.push_str(&format!(
-            "\tinclude_bytes!(\"../../assets/parrot/dark_parrot_{}.ico\"),\n",
-            i
-        ));
-    }
-    file_content.push_str("];\n");
-    file_content.push_str("pub const LIGHT_PARROT: &[&[u8]] = &[\n");
-    for i in 0..10 {
-        file_content.push_str(&format!(
-            "\tinclude_bytes!(\"../../assets/parrot/light_parrot_{}.ico\"),\n",
-            i
-        ));
-    }
-    file_content.push_str("];\n");
+    let mut res = WindowsResource::new();
+    // This path can be absolute, or relative to your crate root.
+    res.set_icon("assets/appIcon.ico");
 
-    let out_dir = std::env::var("OUT_DIR").unwrap();
-    let dest_path = std::path::Path::new(&out_dir).join("icons.rs");
-    std::fs::write(&dest_path, file_content).unwrap();
+    res.compile()?;
+    generate_icon_resources()?;
+    Ok(())
+}
+
+fn generate_icon_resources() -> io::Result<()> {
+    let out_dir = std::env::var_os("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("icons.rs");
+    let themes = ["light", "dark"];
+    let names = [("cat", 5), ("parrot", 10usize)];
+    let mut code = vec![];
+    for theme in themes.iter() {
+        for (name, count) in names.iter() {
+            code.push(generate_icon_resources_array(theme, name, *count));
+        }
+    }
+    std::fs::write(&dest_path, code.join("\n").as_bytes())
+}
+
+fn generate_icon_resources_array(theme: &str, name: &str, cnt: usize) -> String {
+    let base = std::fs::canonicalize(Path::new("assets").join(name)).unwrap();
+    let names = (0..cnt)
+        .map(|i| format!("{}_{}_{}", theme, name, i))
+        .collect::<Vec<_>>();
+    let res = names
+        .iter()
+        .map(|name| {
+            format!(
+                r#"pub const {name}: &[u8] = include_bytes!(r"{fname}.ico");"#,
+                fname = base.join(name).display(),
+                name = name.to_uppercase(),
+            )
+        })
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    format!(
+        r#"
+{res}
+pub const {theme}_{name}: &[&[u8]] = &[
+    {names}
+];
+        "#,
+        res = res,
+        theme = theme.to_uppercase(),
+        name = name.to_uppercase(),
+        names = names.join(",").to_uppercase(),
+    )
 }
